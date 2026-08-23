3a67afd874a30ddae5c8ef78e69dde212b3bd3d5
omralhaj036-collab <omralhaj036@gmail.com>
2026-08-23 08:57:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
 ```javascript
 const CACHE_NAME = 'ojen-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const assets = [
+  '/The-AI-Core/',
+  '/The-AI-Core/index.html',
+  '/The-AI-Core/manifest.json'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
+```
